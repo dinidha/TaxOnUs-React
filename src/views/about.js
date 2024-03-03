563e6bf30b3b5e4a563bbfe40ba53db11ed07326
dinidha <45313850+dinidha@users.noreply.github.com>
2024-03-03 13:24:27 +1000

    Commit from TeleportHQ

--- src/views/about.js
+++ src/views/about.js
@@ -94,6 +94,78 @@ const About = (props) => {
             </div>
           </div>
         </header>
+        <header data-thq="thq-navbar" className="about-navbar-interactive1">
+          <img
+            alt="logo"
+            src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+            className="about-image"
+          />
+          <div data-thq="thq-navbar-nav" className="about-desktop-menu1">
+            <nav className="about-links2">
+              <span>About</span>
+              <span className="about-text01">Features</span>
+              <span className="about-text02">Pricing</span>
+              <span className="about-text03">Team</span>
+              <span className="about-text04">Blog</span>
+            </nav>
+            <div className="about-buttons2">
+              <button className="about-login2 button">Login</button>
+              <button className="button">Register</button>
+            </div>
+          </div>
+          <div data-thq="thq-burger-menu" className="about-burger-menu1">
+            <svg viewBox="0 0 1024 1024" className="about-icon10">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div data-thq="thq-mobile-menu" className="about-mobile-menu">
+            <div className="about-nav1">
+              <div className="about-top1">
+                <img
+                  alt="image"
+                  src="https://presentation-website-assets.teleporthq.io/logos/logo.png"
+                  className="about-logo2"
+                />
+                <div data-thq="thq-close-menu" className="about-close-menu1">
+                  <svg viewBox="0 0 1024 1024" className="about-icon12">
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <nav className="about-links3">
+                <span className="about-text05">About</span>
+                <span className="about-text06">Features</span>
+                <span className="about-text07">Pricing</span>
+                <span className="about-text08">Team</span>
+                <span className="about-text09">Blog</span>
+              </nav>
+              <div className="about-buttons3">
+                <button className="about-login3 button">Login</button>
+                <button className="button">Register</button>
+              </div>
+            </div>
+            <div>
+              <svg
+                viewBox="0 0 950.8571428571428 1024"
+                className="about-icon14"
+              >
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="about-icon16"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 602.2582857142856 1024"
+                className="about-icon18"
+              >
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </header>
       </div>
       <div className="about-hero">
         <div className="about-hero1 heroContainer">
@@ -294,32 +366,32 @@ const About = (props) => {
           <div className="about-container05">
             <div className="freePricingCard about-pricing-card">
               <div className="about-container06">
-                <span className="about-text36 heading3">Free</span>
+                <span className="about-text46 heading3">Free</span>
                 <span className="bodySmall">
                   Ideal for individuals filing simple tax returns
                 </span>
               </div>
               <div className="about-container07">
-                <span className="about-text37">LKR</span>
+                <span className="about-text47">LKR</span>
                 <span className="about-free-plan-price">0</span>
               </div>
               <div className="about-container08">
                 <div className="about-container09">
-                  <span className="about-text38">✔</span>
+                  <span className="about-text48">✔</span>
                   <span className="bodySmall">
                     Basic tax return preparation
                   </span>
                 </div>
                 <div className="about-container10">
-                  <span className="about-text39">✔</span>
+                  <span className="about-text49">✔</span>
                   <span className="bodySmall">E-filing included</span>
                 </div>
                 <div className="about-container11">
-                  <span className="about-text40">✔</span>
+                  <span className="about-text50">✔</span>
                   <span className="bodySmall">Customer support via email</span>
                 </div>
                 <div className="about-container12">
-                  <span className="about-text41">✔</span>
+                  <span className="about-text51">✔</span>
                   <span className="bodySmall">
                     Limited to one state tax return
                   </span>
@@ -331,39 +403,39 @@ const About = (props) => {
             </div>
             <div className="basicPricingCard about-pricing-card1">
               <div className="about-container13">
-                <span className="about-text42 heading3">BASIC</span>
+                <span className="about-text52 heading3">BASIC</span>
                 <span className="bodySmall">
                   Great for families and small businesses
                 </span>
               </div>
               <div className="about-container14">
-                <span className="about-text43">LKR</span>
+                <span className="about-text53">LKR</span>
                 <span className="about-basic-plan-pricing">50</span>
-                <span className="about-text44">/ month</span>
+                <span className="about-text54">/ month</span>
               </div>
               <div className="about-container15">
                 <div className="about-container16">
-                  <span className="about-text45">✔</span>
+                  <span className="about-text55">✔</span>
                   <span className="bodySmall">All features of FREE plan</span>
                 </div>
                 <div className="about-container17">
-                  <span className="about-text47">✔</span>
+                  <span className="about-text57">✔</span>
                   <span className="bodySmall">
                     Tax return preparation for individuals and families
                   </span>
                 </div>
                 <div className="about-container18">
-                  <span className="about-text48">✔</span>
+                  <span className="about-text58">✔</span>
                   <span className="bodySmall">E-filing included</span>
                 </div>
                 <div className="about-container19">
-                  <span className="about-text49">✔</span>
+                  <span className="about-text59">✔</span>
                   <span className="bodySmall">
                     Customer support via email and phone
                   </span>
                 </div>
                 <div className="about-container20">
-                  <span className="about-text50">✔</span>
+                  <span className="about-text60">✔</span>
                   <span className="bodySmall">Up to two state tax returns</span>
                 </div>
               </div>
@@ -373,7 +445,7 @@ const About = (props) => {
             </div>
             <div className="proPricingCard about-pricing-card2">
               <div className="about-container21">
-                <span className="about-text51 heading3">
+                <span className="about-text61 heading3">
                   <span>PRO</span>
                   <br></br>
                 </span>
@@ -382,35 +454,35 @@ const About = (props) => {
                 </span>
               </div>
               <div className="about-container22">
-                <span className="about-text54">LKR</span>
+                <span className="about-text64">LKR</span>
                 <span className="about-pro-plan-pricing">100</span>
-                <span className="about-text55">/ month</span>
+                <span className="about-text65">/ month</span>
               </div>
               <div className="about-container23">
                 <div className="about-container24">
-                  <span className="about-text56">✔</span>
+                  <span className="about-text66">✔</span>
                   <span className="bodySmall"> All features of BASIC plan</span>
                 </div>
                 <div className="about-container25">
-                  <span className="about-text58">✔</span>
+                  <span className="about-text68">✔</span>
                   <span className="bodySmall">
                     Comprehensive tax return preparation for individuals,
                     families, and businesses
                   </span>
                 </div>
                 <div className="about-container26">
-                  <span className="about-text59">✔</span>
+                  <span className="about-text69">✔</span>
                   <span className="bodySmall">E-filing included</span>
                 </div>
                 <div className="about-container27">
-                  <span className="about-text60">✔</span>
+                  <span className="about-text70">✔</span>
                   <span className="bodySmall">
                     Priority customer support via email, phone, and in-person
                     meetings
                   </span>
                 </div>
                 <div className="about-container28">
-                  <span className="about-text61">✔</span>
+                  <span className="about-text71">✔</span>
                   <span className="bodySmall">Unlimited state tax returns</span>
                 </div>
               </div>
@@ -511,8 +583,8 @@ const About = (props) => {
                 <span>FAQ</span>
                 <br></br>
               </span>
-              <h2 className="about-text79 heading2">Common questions</h2>
-              <span className="about-text80 bodyLarge">
+              <h2 className="about-text89 heading2">Common questions</h2>
+              <span className="about-text90 bodyLarge">
                 <span>
                   Here are some of the most common questions that we get.
                 </span>
@@ -548,7 +620,7 @@ const About = (props) => {
         <footer className="footerContainer about-footer1">
           <div className="about-container31">
             <span className="TaxOnUs">TAXPROS</span>
-            <nav className="about-nav1">
+            <nav className="about-nav2">
               <span className="bodySmall">Services</span>
               <span className="about-nav222 bodySmall">About Us</span>
               <span className="about-nav322 bodySmall">Resources</span>
@@ -557,25 +629,25 @@ const About = (props) => {
           </div>
           <div className="about-separator"></div>
           <div className="about-container32">
-            <span className="bodySmall about-text83">
+            <span className="bodySmall about-text93">
               © 2023 myCompany, All Rights Reserved.
             </span>
-            <div className="about-icon-group1">
+            <div className="about-icon-group2">
               <svg
                 viewBox="0 0 950.8571428571428 1024"
-                className="about-icon10 socialIcons"
+                className="about-icon20 socialIcons"
               >
                 <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
               </svg>
               <svg
                 viewBox="0 0 877.7142857142857 1024"
-                className="about-icon12 socialIcons"
+                className="about-icon22 socialIcons"
               >
                 <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
               </svg>
               <svg
                 viewBox="0 0 602.2582857142856 1024"
-                className="about-icon14 socialIcons"
+                className="about-icon24 socialIcons"
               >
                 <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
               </svg>
